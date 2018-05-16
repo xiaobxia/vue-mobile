2c4b53c060f770a7e151844a116fa70a7e643344
chenlingjie <chenlingjie@cd121.com>
2018-05-16 13:58:03 +0800

    xg

--- src/util/httpUtil.js
+++ src/util/httpUtil.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import qs from 'qs'
+import router from '../router/index'
 const basePath = '/myService/'
 
 axios.interceptors.request.use(function (config) {
@@ -12,6 +13,9 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
   if (response.data.success === false) {
     console.error(response.data.message)
+    if (response.data.code === 401) {
+      router.push('/')
+    }
   }
   return response
 }, function (error) {
