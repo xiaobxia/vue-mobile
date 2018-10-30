657cffacdcead51a969fde09458d052c11b8b837
xiaobxia <673806687@qq.com>
2018-10-30 13:06:42 +0800

    xg

--- src/routers/TodayIndex/index.vue
+++ src/routers/TodayIndex/index.vue
@@ -23,7 +23,6 @@
 
 <script>
 import Http from '@/util/httpUtil.js'
-import indexInfoUtil from '@/util/indexInfoUtil.js'
 import qs from 'qs'
 import numberUtil from '@/util/numberUtil.js'
 import storageUtil from '@/util/storageUtil.js'
@@ -35,7 +34,96 @@ const dataRawList = {
   '东方': 'getWebStockdaybarTodayDongfang'
 }
 
-const codeMap = indexInfoUtil.codeMap
+const codeMap = {
+  'chuangye': {
+    code: 'sz399006',
+    name: '创业'
+  },
+  'gangtie': {
+    code: 'sz399440',
+    name: '钢铁'
+  },
+  'jungong': {
+    code: 'sz399959',
+    name: '军工'
+  },
+  'yiyao': {
+    code: 'sh000037',
+    name: '医药'
+  },
+  'meitan': {
+    code: 'sz399998',
+    name: '煤炭'
+  },
+  'youse': {
+    code: 'sh000823',
+    name: '有色'
+  },
+  'jisuanji': {
+    code: 'sz399363',
+    name: '计算机'
+  },
+  'baijiu': {
+    code: 'sz399997',
+    name: '白酒'
+  },
+  'xinxi': {
+    code: 'sh000993',
+    name: '信息'
+  },
+  'xiaofei': {
+    code: 'sh000990',
+    name: '消费'
+  },
+  'baoxian': {
+    code: 'sz399809',
+    name: '保险'
+  },
+  'wulin': {
+    code: 'sh000016',
+    name: '50'
+  },
+  'chuanmei': {
+    code: 'sz399971',
+    name: '传媒'
+  },
+  'dianzi': {
+    code: 'sz399811',
+    name: '电子'
+  },
+  'yiliao': {
+    code: 'sz399989',
+    name: '医疗'
+  },
+  'shengwu': {
+    code: 'sz399441',
+    name: '生物'
+  },
+  'sanbai': {
+    code: 'sh000300',
+    name: '300'
+  },
+  'wubai': {
+    code: 'sh000905',
+    name: '500'
+  },
+  'yinhang': {
+    code: 'sz399986',
+    name: '银行'
+  },
+  'dichan': {
+    code: 'sz399393',
+    name: '地产'
+  },
+  'huanbao': {
+    code: 'sh000827',
+    name: '环保'
+  },
+  'shangzheng': {
+    code: 'sh000001',
+    name: '上证'
+  }
+}
 export default {
   name: 'OperatingInfo',
   data () {
