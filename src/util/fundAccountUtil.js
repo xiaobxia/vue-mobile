299b9700d29cb3bb2aec9af7f4e067a83309ea9d
xiaobxia <673806687@qq.com>
2018-11-21 09:42:57 +0800

    xg

--- src/util/fundAccountUtil.js
+++ src/util/fundAccountUtil.js
@@ -1,20 +1,10 @@
 /**
  * Created by xiaobxia on 2018/5/30.
  */
-// 资产
-const asset = 200000
-// 标准仓金额
-const standard = 5000
-
 const minHasDay = 5
 
 const fundAccountUtil = {
-  asset,
   myShares: 200000,
-  buyIn7DaysLimit: asset / 4,
-  standard,
-  cutRateLevelOne: 0.8,
-  cutRateLevelTwo: 0.6,
   minHasDay,
   // 解除
   ifRelieve: function (item) {
